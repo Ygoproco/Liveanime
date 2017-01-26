2001dac42804f50009688f4901c3a420e366a404
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-01-26 09:10:14 +0800

    D - cards fixes (511009446/7)

--- script/c511009447.lua
+++ script/c511009447.lua
@@ -1,36 +1,29 @@
 --D - Hyper Nova
-function c511009457.initial_effect(c)
+function c511009447.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetHintTiming(0,0x1e0)
-	e1:SetCost(c511009457.cost)
-	e1:SetTarget(c511009457.target)
-	e1:SetOperation(c511009457.activate)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCondition(c511009447.condition)
+	e1:SetTarget(c511009447.target)
+	e1:SetOperation(c511009447.activate)
 	c:RegisterEffect(e1)
 end
-function c511009457.filter(c)
-	return c:IsSetCard(0xc008)
+function c511009447.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xc008)
 end
-function c511009457.ctfilter(c)
-	return bit.band(c:GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL
+function c511009447.condition(e,tp,eg,ev,ep,re,r,rp)
+	return eg:IsExists(Card.IsControler,1,nil,1-tp) and Duel.IsExistingMatchingCard(Card.IsSummonType,tp,0,LOCATION_MZONE,1,nil,SUMMON_TYPE_SPECIAL)
+		and Duel.IsExistingMatchingCard(c511009447.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-function c511009457.descon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp and eg:IsExists(Card.IsControler,1,nil,1-tp)
-	and Duel.IsExistingMatchingCard(c511009457.filter,tp,LOCATION_MZONE,0,1,nil)
-	and Duel.IsExistingMatchingCard(c511009457.ctfilter,tp,0,LOCATION_MZONE,1,nil)
-end
-function c511009457.dfilter(c)
-	return c:IsStatus(STATUS_SPSUMMON_TURN)
-end
-function c511009457.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c511009457.dfilter,tp,0,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(c511009457.dfilter,tp,0,LOCATION_MZONE,nil)
+function c511009447.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsStatus,tp,0,LOCATION_MZONE,1,nil,STATUS_SPSUMMON_TURN) end
+	local g=Duel.GetMatchingGroup(Card.IsStatus,tp,0,LOCATION_MZONE,nil,STATUS_SPSUMMON_TURN)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
-function c511009457.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c511009457.dfilter,tp,0,LOCATION_MZONE,nil)
+function c511009447.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsStatus,tp,0,LOCATION_MZONE,nil,STATUS_SPSUMMON_TURN)
 	Duel.Destroy(g,REASON_EFFECT)
 end
