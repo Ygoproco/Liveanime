2719b0147bea77b7d7c8532909c32e963ba4a7ac
ClementLouis <clementlouis.gallier@viacesi.fr>
2017-01-31 21:31:28 +0100

    Supreme King Violent Spirit
    removed a part useless because included in the proc

--- script/c511009518.lua
+++ script/c511009518.lua
@@ -6,16 +6,6 @@ function c511009518.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
 	e2:SetCode(511009518)
 	c:RegisterEffect(e2)
-	--Equip limit
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_EQUIP_LIMIT)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetValue(c511009518.eqlimit)
-	c:RegisterEffect(e3)
-end
-function c511009518.eqlimit(e,c)
-	return c:IsSetCard(0xf8) 
 end
 function c511009518.filter(c)
 	return c:IsSetCard(0xf8) and c:IsFaceup() 
