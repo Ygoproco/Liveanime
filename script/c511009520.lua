4ebdc76ba82354d09b75f1948740941d824bcf40
ClementLouis <clementlouis.gallier@viacesi.fr>
2017-02-03 09:44:31 +0100

    Superheavy Samurai Swordsmith
    Fixed ID

--- script/c511009520.lua
+++ script/c511009520.lua
@@ -1,5 +1,5 @@
 --Superheavy Samurai Swordsmith
-function c36953371.initial_effect(c)
+function c511009520.initial_effect(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_MACHINE),aux.NonTuner(Card.IsSetCard,0x9a),1)
 	c:EnableReviveLimit()
@@ -14,20 +14,20 @@ function c36953371.initial_effect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetTarget(c3493978.target)
-	e2:SetOperation(c3493978.operation)
+	e2:SetTarget(c511009520.target)
+	e2:SetOperation(c511009520.operation)
 	c:RegisterEffect(e2)
 end
 
-function c3493978.filter(c,e,tp)
+function c511009520.filter(c,e,tp)
 	return c:IsSetCard(0x9a) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c3493978.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c511009520.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c3493978.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c511009520.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c3493978.operation(e,tp,eg,ep,ev,re,r,rp)
+function c511009520.operation(e,tp,eg,ep,ev,re,r,rp)
 	local ct=2
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ct=1 end
 	ct=math.min(ct,Duel.GetLocationCount(tp,LOCATION_MZONE))
