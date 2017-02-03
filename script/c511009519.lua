239ddfc1962e96892975310dcbdf9464e111af81
ClementLouis <clementlouis.gallier@viacesi.fr>
2017-02-03 10:21:04 +0100

    Superheavy Samurai Bushin Fudomyo'o

--- script/c511009519.lua
+++ script/c511009519.lua
@@ -1,5 +1,5 @@
---スターダスト・ドラゴン
-function c44508094.initial_effect(c)
+--Superheavy Samurai Bushin Fudomyo'o
+function c511009519.initial_effect(c)
 	--Negate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(44508094,0))
@@ -8,43 +8,84 @@ function c44508094.initial_effect(c)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c44508094.condition)
-	e1:SetTarget(c44508094.target)
-	e1:SetOperation(c44508094.operation)
+	e1:SetCondition(c511009519.condition)
+	e1:SetTarget(c511009519.target)
+	e1:SetOperation(c511009519.operation)
 	c:RegisterEffect(e1)
-	--Revive
+	
+	--protection
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(44508094,1))
-	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetRange(LOCATION_GRAVE)
+	e2:SetDescription(aux.Stringid(65195959,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1)
-	e2:SetTarget(c44508094.sumtg)
-	e2:SetOperation(c44508094.sumop)
+	e2:SetTarget(c65195959.protg)
+	e2:SetOperation(c511009519.proop)
 	c:RegisterEffect(e2)
+	
+	--
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetCode(EFFECT_DEFENSE_ATTACK)
+	e8:SetValue(1)
+	c:RegisterEffect(e8)
 end
-function c511000979.cfilter(c,tp)
+function c511009519.cfilter(c,tp)
 	return c:IsOnField() and c:IsControler(tp)
 end
-function c44508094.condition(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or not Duel.IsChainNegatable(ev) then return false end
+function c511009519.condition(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
 	if re:IsHasCategory(CATEGORY_NEGATE) and (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE)
 		and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
 	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
 	return ex and tg~=nil and tc+tg:FilterCount(c511000979.cfilter,nil,tp)-tg:GetCount()>0
 end
-function c42880485.filter(c)
+
+
+function c511009519.protg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c511009519.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,0,1,2,nil)
+end
+function c511009519.proop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	local tc=g:GetFirst()
+	while tc do
+		c:SetCardTarget(tc)
+		
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_OWNER_RELATE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1:SetCondition(c38247752.rcon)
+		tc:RegisterEffect(e1,true)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		c:RegisterEffect(e2,true)
+		tc=g:GetNext()
+	end
+end
+
+function c511009519.rcon(e)
+	return e:GetOwner():IsHasCardTarget(e:GetHandler())
+end
+
+function c511009519.filter(c)
 	return c:IsFaceup() 
 end
-function c44508094.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c42880485.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c42880485.filter,tp,LOCATION_MZONE,0,1,nil) end
+function c511009519.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c511009519.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c511009519.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,c42880485.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,c511009519.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c44508094.operation(e,tp,eg,ep,ev,re,r,rp)
+function c511009519.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if not Duel.CheckReleaseGroup(tp,Card.IsSetCard,1,nil,0x9a) then return end
 	local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,1,1,nil,0x9a)
