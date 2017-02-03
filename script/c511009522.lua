5479ad0fe8d96661e764302e3cfe3d4f297b9117
ClementLouis <clementlouis.gallier@viacesi.fr>
2017-02-03 10:04:00 +0100

    Supreme King Servant Dragon Odd eyes
    Added pendulum type and fixed location of summon effect

--- script/c511009522.lua
+++ script/c511009522.lua
@@ -114,15 +114,15 @@ function c511009522.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoExtraP(e:GetHandler(),tp,REASON_COST)
 end
 function c511009522.spfilter(c,e,tp)
-	return c:IsFaceup() and c:IsSetCard(0x20f8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsFaceup() and c:IsSetCard(0x20f8) and c:IsType(TYPE_PENDULUM)  and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c511009522.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
-		and Duel.IsExistingTarget(c511009522.spfilter,tp,LOCATION_GRAVE,0,2,nil,e,tp) end
+		and Duel.IsExistingTarget(c511009522.spfilter,tp,LOCATION_EXTRA,0,2,e:GetHandler(),e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c511009522.spfilter,tp,LOCATION_GRAVE,0,2,2,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c511009522.spfilter,tp,LOCATION_EXTRA,0,2,2,e:GetHandler(),e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,2,0,0)
 end
 
