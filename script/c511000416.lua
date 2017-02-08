3b29f0d4c6c08a7aabc9bcba0cedd18102e098b8
Ygoproco <Ygoproco@users.noreply.github.com>
2017-02-08 23:17:26 +0100

    Enchanted Sword Nothung fix

--- script/c511000416.lua
+++ script/c511000416.lua
@@ -1,15 +1,6 @@
 --Enchanted Sword Nothung
 function c511000416.initial_effect(c)
 	aux.AddEquipProcedure(c,nil,nil,nil,nil,c511000416.tg,c511000416.op)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_EQUIP+CATEGORY_DESTROY+CATEGORY_REMOVE)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(c511000416.target)
-	e1:SetOperation(c511000416.operation)
-	c:RegisterEffect(e1)
 	--Atk
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
