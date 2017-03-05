c491261792927456399033b2ecb5589c81e20cfc
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-03-06 01:47:25 +0800

    Script Revert

--- script/c100000297.lua
+++ script/c100000297.lua
@@ -1,27 +1,12 @@
 --エアロの爪
 function c100000297.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(c100000297.target)
-	e1:SetOperation(c100000297.operation)
-	c:RegisterEffect(e1)
+	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsCode,76812113))
 	--Atk up
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetValue(300)
 	c:RegisterEffect(e2)
-	--Equip limit
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_EQUIP_LIMIT)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetValue(c100000297.eqlimit)
-	c:RegisterEffect(e3)
 	--tograve replace
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
@@ -31,25 +16,6 @@ function c100000297.initial_effect(c)
 	e4:SetTarget(c100000297.reptg)
 	e4:SetOperation(c100000297.repop)
 	c:RegisterEffect(e4)
-end
-function c100000297.eqlimit(e,c)
-	return c:IsCode(76812113)
-end
-function c100000297.filter(c)
-	return c:IsFaceup() and c:IsCode(76812113)
-end
-function c100000297.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:GetLocation()==LOCATION_MZONE and c100000297.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c100000297.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,c100000297.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
-end
-function c100000297.operation(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.Equip(tp,e:GetHandler(),tc)
-	end
 end
 function c100000297.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
