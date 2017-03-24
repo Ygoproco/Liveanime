76cf63d7f5a189a3cb40b1f2af80ced8fc7bb24b
ClementLouis <clementlouis.gallier@viacesi.fr>
2017-03-24 16:29:32 +0100

    Update c511009526.lua

--- script/c511009526.lua
+++ script/c511009526.lua
@@ -36,7 +36,7 @@ function c511009526.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c511009526.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) then
+	if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
