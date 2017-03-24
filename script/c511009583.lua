cd6a638115270a6a3f298bbd9cb196e7117218f9
ClementLouis <clementlouis.gallier@viacesi.fr>
2017-03-24 16:38:52 +0100

    Update c511009583.lua

--- script/c511009583.lua
+++ script/c511009583.lua
@@ -12,8 +12,22 @@ function c511009583.initial_effect(c)
 	c:RegisterEffect(e1)
 	
 end
+--White monster collection
+c511009126.collection={
+	[1571945]=true;[3557275]=true;[9433350]=true;[13429800]=true;
+	[15150365]=true;[20193924]=true;[24644634]=true;[32269855]=true;
+	[38517737]=true;[73398797]=true;[73891874]=true;[79473793]=true;
+	[79814787]=true;[89631139]=true;[92409659]=true;[98024118]=true;
+	[22804410]=true;[71039903]=true;[84812868]=true;
+	[501000016]=true;
+	[511002341]=true;
+	[511001977]=true;[511001978]=true;[511001979]=true;[511001980]=true;
+	[511001090]=true;[511001091]=true;	
+	[95100846]=true;[95100847]=true;
+	
+}
 function c511009583.cfilter(c)
-	return c:IsSetCard(0x202) and c:IsDiscardable()
+	return (c511009126.collection[c:GetCode()] or c:IsSetCard(0x202)) and c:IsType(TYPE_MONSTER) and c:IsDiscardable()
 end
 function c511009583.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c511009583.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
