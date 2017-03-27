21c24fcaa896bea0f842b1f7df8f44ccc065be88
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-03-26 22:31:39 -0300

    fix
    small fix

--- script/c511004443.lua
+++ script/c511004443.lua
@@ -52,7 +52,7 @@ function c511004443.operation(e,tp,eg,ev,ep,re,r,rp)
 		local mg=tg:Clone()
 		local tc=tg:GetFirst()
 		while tc do
-			if tc:GetOverlayCount()~=0 then mg:Merge(tc:GetOverlayGroup()) end
+			if tc:GetOverlayCount()~=0 then Duel.SendtoGrave(tc:GetOverlayGroup(),REASON_RULE) end
 			tc=tg:GetNext()
 		end
 		c:SetMaterial(mg)
