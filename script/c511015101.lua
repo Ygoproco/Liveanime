071fbd60290e4ca198ac9600d8a58873c55f55aa
Sahim <loris.cangini@hotmail.it>
2017-03-28 21:46:09 +0200

    Updated "D/D/D Fusion" (#95)

--- script/c511015101.lua
+++ script/c511015101.lua
@@ -9,35 +9,47 @@ function c511015101.initial_effect(c)
 	e1:SetOperation(c511015101.activate)
 	c:RegisterEffect(e1)
 end
-function c511015101.subcon(e)
-	return e:GetHandler():IsLocation(LOCATION_ONFIELD+LOCATION_HAND)
-end
+
 function c511015101.filter1(c,e)
-	return not c:IsImmuneToEffect(e)
+	return c:IsCanBeFusionMaterial() and not c:IsImmuneToEffect(e)
 end
 function c511015101.filter2(c,e,tp,m,f,chkf)
 	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x10af) and (not f or f(c))
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
 end
-function c511015101.filter3(c,e,tp,m,f,chkf)
-	return c511015101.filter2(c,e,tp,m,f,chkf) and aux.IsMaterialListCode(c,47198668)
+function c511015101.filter3(c,e,tp,m,f,chkf,token)
+	return c511015101.filter2(c,e,tp,m,f,chkf) 
+		and token and token:IsCanBeFusionMaterial(f)
+end
+
+function c511015101.tokenOp(e,tp,eg,ep,ev,re,r,rp)
+	e:SetLabelObject(Duel.CreateToken(tp,47198668))
+end
+function c511015101.subcon(e)
+	return e:GetHandler():IsLocation(LOCATION_ONFIELD+LOCATION_HAND)
 end
 function c511015101.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local chkf=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and PLAYER_NONE or tp
 		local mg1=Duel.GetFusionMaterial(tp)
 		local res1=Duel.IsExistingMatchingCard(c511015101.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
-		mg1:AddCard(e:GetHandler()) 
+		
+		local c=e:GetHandler()
+		mg1:AddCard(c) 	
+
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetOperation(c511015101.tokenOp)
+		c:RegisterEffect(e1)
 		
 		--fusion substitute
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_FUSION_SUBSTITUTE)
-		e1:SetCondition(c511015101.subcon)
-		e1:SetReset(RESET_EVENT+EVENT_LEAVE_FIELD)
-		e:GetHandler():RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_FUSION_SUBSTITUTE)
+		e2:SetCondition(c511015101.subcon)
+		c:RegisterEffect(e2)
 		
-		local res2=Duel.IsExistingMatchingCard(c511015101.filter3,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+		local res2=Duel.IsExistingMatchingCard(c511015101.filter3,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf,e1:GetLabelObject())
 		if not (res1 or res2) then
 			local ce=Duel.GetChainMaterial(tp)
 			if ce~=nil then
@@ -56,7 +68,8 @@ function c511015101.activate(e,tp,eg,ep,ev,re,r,rp)
 	local mg1=Duel.GetFusionMaterial(tp):Filter(c511015101.filter1,nil,e)
 	local sg1=Duel.GetMatchingGroup(c511015101.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
 	mg1:AddCard(e:GetHandler()) 
-	sg1:Merge(Duel.GetMatchingGroup(c511015101.filter3,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf))
+	local token=Duel.CreateToken(tp,47198668)
+	sg1:Merge(Duel.GetMatchingGroup(c511015101.filter3,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf,token))
 	local mg2=nil
 	local sg2=nil
 	local ce=Duel.GetChainMaterial(tp)
@@ -73,7 +86,7 @@ function c511015101.activate(e,tp,eg,ep,ev,re,r,rp)
 		local tg=sg:Select(tp,1,1,nil)
 		local tc=tg:GetFirst()
 		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			if not aux.IsMaterialListCode(tc,47198668) then
+			if not token:IsCanBeFusionMaterial(tc) then
 				mg1:RemoveCard(e:GetHandler())  
 			end
 			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
