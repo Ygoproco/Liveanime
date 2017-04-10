7861eacd771e1e48e04e8575e2860c15ad178c13
ClementLouis <clementlouis.gallier@viacesi.fr>
2017-04-10 19:16:40 +0200

    White Stungray update
    Update to use the archtype token

--- script/c511009583.lua
+++ script/c511009583.lua
@@ -10,24 +10,27 @@ function c511009583.initial_effect(c)
 	e1:SetTarget(c511009583.sptg)
 	e1:SetOperation(c511009583.spop)
 	c:RegisterEffect(e1)
-	
+	if not c511009583.global_check then
+		c511009583.global_check=true
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_ADJUST)
+		ge2:SetCountLimit(1)
+		ge2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+		ge2:SetOperation(c511009583.archchk)
+		Duel.RegisterEffect(ge2,0)
+	end
 end
---White monster collection
-c511009126.collection={
-	[1571945]=true;[3557275]=true;[9433350]=true;[13429800]=true;
-	[15150365]=true;[20193924]=true;[24644634]=true;[32269855]=true;
-	[38517737]=true;[73398797]=true;[73891874]=true;[79473793]=true;
-	[79814787]=true;[89631139]=true;[92409659]=true;[98024118]=true;
-	[22804410]=true;[71039903]=true;[84812868]=true;
-	[501000016]=true;
-	[511002341]=true;
-	[511001977]=true;[511001978]=true;[511001979]=true;[511001980]=true;
-	[511001090]=true;[511001091]=true;	
-	[95100846]=true;[95100847]=true;
-	
-}
+function c511009583.archchk(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(0,420)==0 then 
+		Duel.CreateToken(tp,420)
+		Duel.CreateToken(1-tp,420)
+		Duel.RegisterFlagEffect(0,420,0,0,0)
+	end
+end
+
 function c511009583.cfilter(c)
-	return (c511009126.collection[c:GetCode()] or c:IsSetCard(0x202)) and c:IsType(TYPE_MONSTER) and c:IsDiscardable()
+	return c420.IsWhite(c) and c:IsType(TYPE_MONSTER) and c:IsDiscardable()
 end
 function c511009583.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c511009583.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
