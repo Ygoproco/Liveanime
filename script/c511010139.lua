d8d9485edb8774c6e502cb997f521c2902ba1bd4
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-04-12 13:45:29 +0800

    Number C32 and Number C39 Xyz Proc update

--- script/c511010139.lua
+++ script/c511010139.lua
@@ -1,8 +1,20 @@
 --CNo.39 希望皇ホープレイ (Anime)
+--fixed by MLD
 function c511010139.initial_effect(c)
 	--xyz summon
 	c:EnableReviveLimit()
-	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_LIGHT),4,3,c511010139.ovfilter,aux.Stringid(511010139,1))
+	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_LIGHT),4,3)
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetDescription(aux.Stringid(56840427,1))
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetCondition(aux.XyzCondition2(c511010139.ovfilter))
+	e0:SetTarget(aux.XyzTarget2(c511010139.ovfilter))
+	e0:SetOperation(c511010139.xyzop)
+	e0:SetValue(SUMMON_TYPE_XYZ)
+	c:RegisterEffect(e0)
 	--attack up
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
@@ -10,8 +22,9 @@ function c511010139.initial_effect(c)
 	e1:SetProperty(0,EFFECT_FLAG2_XMDETACH)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCost(c511010139.cost)
-	e1:SetOperation(c511010139.operation)
+	e1:SetCost(c511010139.atkcost)
+	e1:SetTarget(c511010139.atktg)
+	e1:SetOperation(c511010139.atkop)
 	c:RegisterEffect(e1)
 	--selfdes
 	local e2=Effect.CreateEffect(c)
@@ -19,7 +32,7 @@ function c511010139.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetCode(EFFECT_SELF_DESTROY)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(c511010139.condition)
+	e2:SetCondition(c511010139.descon)
 	c:RegisterEffect(e2)
 	--battle indestructable
 	local e6=Effect.CreateEffect(c)
@@ -42,11 +55,27 @@ c511010139.xyz_number=39
 function c511010139.ovfilter(c)
 	return c:IsFaceup() and c:IsCode(84013237)
 end
-function c511010139.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c511010139.xyzop(e,tp,eg,ep,ev,re,r,rp,c,og,min,max)
+	local g=e:GetLabelObject()
+	local mg2=g:GetFirst():GetOverlayGroup()
+	if mg2:GetCount()~=0 then
+		Duel.Overlay(c,mg2)
+	end
+	c:SetMaterial(g)
+	Duel.Overlay(c,g)
+	if not g:GetFirst():IsCode(84013237) then
+		Duel.SendtoGrave(mg2,REASON_RULE)
+	end
+	g:DeleteGroup()
+end
+function c511010139.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function c511010139.operation(e,tp,eg,ep,ev,re,r,rp)
+function c511010139.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+end
+function c511010139.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		local e1=Effect.CreateEffect(c)
@@ -58,24 +87,25 @@ function c511010139.operation(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e1)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-		if g:GetCount()>0 then
+		local tc=g:GetFirst()
+		if tc then
 			Duel.HintSelection(g)
 			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetCode(EFFECT_UPDATE_ATTACK)
 			e2:SetValue(-1000)
 			e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-			g:GetFirst():RegisterEffect(e2)
+			tc:RegisterEffect(e2)
 		end
 	end
 end
-function c511010139.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetLP(tp)>=1000
+function c511010139.descon(e)
+	return Duel.GetLP(e:GetHandlerPlayer())>=1000
 end
 function c511010139.numchk(e,tp,eg,ep,ev,re,r,rp)
-	Duel.CreateToken(tp,84124261)
-	Duel.CreateToken(1-tp,84124261)
+	Duel.CreateToken(tp,56840427)
+	Duel.CreateToken(1-tp,56840427)
 end
 function c511010139.indes(e,c)
-return not c:IsSetCard(0x48)
+	return not c:IsSetCard(0x48)
 end
