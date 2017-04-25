c6627fae096621f272da289b146997a010b21921
Sahim <loris.cangini@hotmail.it>
2017-04-25 22:50:16 +0200

    New Cards & fixes (#153)
    * New Cards
    
    Pendulum Transfer
    Helpoemer (Manga)
    
    * New Card
    
    Elemental HERO Neo Bubbleman (anime)
    
    * fixed "Seven Sword Warrior (manga)" anime status

---
+++ script/c511015128.lua
@@ -0,0 +1,42 @@
+--Elemental HERO Neo Bubbleman (anime)
+function c511015128.initial_effect(c)
+	c:EnableReviveLimit()
+	--spsummon condition
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e2:SetValue(c511015128.splimit)
+	c:RegisterEffect(e2)
+	--change code
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CHANGE_CODE)
+	e3:SetValue(79979666)
+	c:RegisterEffect(e3)
+	--destroy
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(511015128,0))
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_DAMAGE_STEP_END)
+	e4:SetTarget(c511015128.destg)
+	e4:SetOperation(c511015128.desop)
+	c:RegisterEffect(e4)
+end
+function c511015128.splimit(e,se,sp,st)
+	return se:GetHandler():IsCode(46411259)
+end
+function c511015128.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local bc=e:GetHandler():GetBattleTarget()
+	if chk==0 then return bc and bc:IsRelateToBattle() end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
+end
+function c511015128.desop(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	if bc:IsRelateToBattle() then
+		Duel.Destroy(bc,REASON_EFFECT)
+	end
+end
