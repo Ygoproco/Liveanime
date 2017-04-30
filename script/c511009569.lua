2a1fe6a55609cca044c83fb2c49df913bebe53b3
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-05-01 01:19:27 +0800

    Database Merge and Fixes, New Cards by R.A.Z.E.R.,
    New Cards include:
    * Extra Veiler (Anime)
    * Exploder Dragonwing (Anime)
    
     Anime Cards aliases updated to official card number
    Card Names Update including Lyrilusc, Supreme King Dragon,

--- script/c511009569.lua
+++ script/c511009569.lua
@@ -1,4 +1,5 @@
 --Raidraptor - Rudder Strix
+--fixed by MLD
 function c511009569.initial_effect(c)
 	--damage
 	local e1=Effect.CreateEffect(c)
@@ -6,7 +7,7 @@ function c511009569.initial_effect(c)
 	e1:SetDescription(aux.Stringid(114932,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetTarget(c511009569.damtg)
 	e1:SetOperation(c511009569.damop)
 	c:RegisterEffect(e1)
@@ -18,14 +19,11 @@ function c511009569.initial_effect(c)
 	e3:SetDescription(aux.Stringid(8785161,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_BE_BATTLE_TARGET)
-	e3:SetCountLimit(1)
 	e3:SetTarget(c511009569.sptg)
 	e3:SetOperation(c511009569.spop)
 	c:RegisterEffect(e3)
 end
-
 function c511009569.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
@@ -36,13 +34,11 @@ function c511009569.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
-
 function c511009569.spfilter(c,e,tp)
 	return c:IsSetCard(0xba) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c511009569.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1 
 		and Duel.IsExistingMatchingCard(c511009569.spfilter,tp,LOCATION_HAND,0,2,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND)
 end
