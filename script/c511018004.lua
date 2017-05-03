633a9908364d24f9f07941d2ccd566208ff6115f
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-05-03 09:53:54 -0300

    Update c511018004.lua

--- script/c511018004.lua
+++ script/c511018004.lua
@@ -2,7 +2,6 @@
 function c511018004.initial_effect(c)
 	--
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c511018004.target)
