1e945c73e6256fb57fdfe1df2d9b65ce76eff552
Sahim <loris.cangini@hotmail.it>
2017-05-06 14:45:38 +0200

    New Manga Cards
    Doll House
    No. 13: Cain's Doom
    No. 31: Abel's Doom

---
+++ script/c511015133.lua
@@ -0,0 +1,69 @@
+--No. 31: Abel's Doom
+function c511015133.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,1,2)
+	c:EnableReviveLimit()
+	--summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_DAMAGE)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c511015133.con)
+	e1:SetOperation(c511015133.op)
+	c:RegisterEffect(e1)
+	--Destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(c511015133.desreptg)
+	e2:SetOperation(c511015133.desrepop)
+	c:RegisterEffect(e2)
+	
+	if not c511015133.global_check then
+		c511015133.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_PREDRAW)
+		ge1:SetCountLimit(1)
+		ge1:SetOperation(c511015133.readLP)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+c511015133.xyz_number=31
+c511015133.LP=-1
+function c511015133.readLP(e,tp,eg,ev,ep,re,r,rp)
+	c511015133.LP=Duel.GetLP(tp)
+	Debug.Message(c511015133.LP)
+	e:Reset()
+end
+
+function c511015133.filter(c)
+	return c:IsFaceup() and c:IsCode(69058960)
+end
+
+function c511015133.con(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return ft>0 and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_EXTRA,0,1,nil,69058960) and 
+		Duel.GetLP(tp)<=c511015133.LP/2 and Duel.GetLP(1-tp)<=c511015133.LP/2
+end
+function c511015133.op(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,true,false,POS_FACEUP)
+		Duel.Destroy(g,REASON_EFFECT)
+		Duel.SetLP(tp,1000)
+	end
+end
+
+function c511015133.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsOnField() and c:IsFaceup() end
+	return Duel.IsExistingMatchingCard(c511015133.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+end
+function c511015133.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Damage(1-tp,e:GetHandler():GetAttack(),REASON_EFFECT)
+end
