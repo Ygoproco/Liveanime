260f2cd10d6aa7e7ae17197f649022f9b7db31eb
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-05-24 15:43:31 -0300

    fix for Net Resonator
    eg was nil (also no damage effect was no working)

--- script/c511009513.lua
+++ script/c511009513.lua
@@ -13,21 +13,28 @@ function c511009513.condition(e,tp,eg,ep,ev,re,r,rp)
 	return r==REASON_SYNCHRO
 end
 function c511009513.operation(e,tp,eg,ep,ev,re,r,rp)
-	local rc=eg:GetFirst()
-	while rc do
-		if rc:GetFlagEffect(511009513)==0 then
-			--cannot special summon
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetDescription(aux.Stringid(511009513,0))
-			e1:SetProperty(EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_PLAYER_TARGET)
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetCode(EFFECT_NO_EFFECT_DAMAGE)
-			e1:SetReset(RESET_EVENT+0x1fe0000)
-			e1:SetAbsoluteRange(ep,1,0)
-			rc:RegisterEffect(e1,true)
-			rc:RegisterFlagEffect(511009513,RESET_EVENT+0x1fe0000,0,1)
-		end
-		rc=eg:GetNext()
+local c=e:GetHandler()
+local rc=c:GetReasonCard()
+if rc:GetFlagEffect(511009513)==0 then
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(511009513,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_ABSOLUTE_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetAbsoluteRange(ep,1,0)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	e1:SetValue(c511009513.damval)
+	e1:SetLabel(ep)
+	rc:RegisterEffect(e1,true)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	rc:RegisterEffect(e2,true)
+	rc:RegisterFlagEffect(511009513,RESET_EVENT+0x1fe0000,0,1)
 	end
+end
+function c511009513.damval(e,re,val,r,rp,rc)
+	if bit.band(r,REASON_EFFECT)~=0 and e:GetHandler():GetControler()==e:GetLabel() then return 0 end
+	return val
 end
