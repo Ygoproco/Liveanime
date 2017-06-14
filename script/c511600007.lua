6a0a77c6d63d9bc520f8bc513c78c6752cdabb0f
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-06-15 03:57:47 +0800

    Scripts and Database Fixes (#222)
    * Doll of Demise alias, name, OT change
    
    * Guardian Formation activatable fix
    
    * Blue Flame Swordsman Level fix
    
    * Cloning fix
    
    * Magnet Warrior Plus/Minus type change

--- script/c511600007.lua
+++ script/c511600007.lua
@@ -1,5 +1,6 @@
 --Cloning (Anime)
 --scripted by Larry126
+--fixed by MLD
 function c511600007.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -12,31 +13,26 @@ function c511600007.initial_effect(c)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCondition(c511600007.condition2)
 	c:RegisterEffect(e2)
 	local e3=e1:Clone()
 	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e3)	
 end
 function c511600007.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and eg:GetCount()==1
-end
-function c511600007.cfilter(c,tp)
-	return c:GetSummonPlayer()==1-tp
-end
-function c511600007.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c511600007.cfilter,1,nil,tp)
+	local tc=eg:GetFirst()
+	return eg:GetCount()==1 and tc:GetSummonPlayer()==1-tp
 end
 function c511600007.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ec=eg:GetFirst()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and 
-		Duel.IsPlayerCanSpecialSummo(tp,ec:GetOriginalCode(),0,ec:GetOriginalType(),ec:GetTextAttack(),ec:GetTextDefense(),
-			ec:GetOriginalLevel()+ec:GetOriginalRank(),ec:GetOriginalRace(),ec:GetOriginalAttribute()) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,ec:GetOriginalCode(),0,ec:GetOriginalType(),ec:GetTextAttack(),ec:GetTextDefense(),
+		ec:GetOriginalLevel()+ec:GetOriginalRank(),ec:GetOriginalRace(),ec:GetOriginalAttribute()) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,nil)
 end
 function c511600007.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local ec=eg:GetFirst()
-	local c=Duel.CreateToken(tp,ec:GetOriginalCode())
-	Duel.SpecialSummon(c,SUMMON_TYPE_SPECIAL,tp,tp.false,false,POS_FACEUP)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,ec:GetOriginalCode(),0,ec:GetOriginalType(),ec:GetTextAttack(),ec:GetTextDefense(),
+		ec:GetOriginalLevel()+ec:GetOriginalRank(),ec:GetOriginalRace(),ec:GetOriginalAttribute()) then return end
+	local tc=Duel.CreateToken(tp,ec:GetOriginalCode())
+	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 end
