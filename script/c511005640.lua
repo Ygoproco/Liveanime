8f0e369f370787fca7a22d1ab362b5ff5e220ef7
GameMaster719 <cobaltss87@yahoo.com>
2017-07-13 13:54:18 -0600

    New Anime cards (#259)
    * New Anime cards 
    
    New anime cards and 1 513 missing script==attribute bomb anime
    *Clear wall
    *Arcana Force Ex the Dark Ruler
    *The Selection
    *Last Resort
    *Soul Demoltion
    *Dandylion
    *OverLimit
    *D hero Doom lord
    
    * Cdb For merge
    
    Cdb For merge new anime cards

---
+++ script/c511005640.lua
@@ -0,0 +1,58 @@
+--D－HERO Doom Lord (Anime)
+--scripted by GameMaster(GM)
+function c511005640.initial_effect(c)
+	--remove
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(511005640,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(c511005640.cost)
+	e1:SetTarget(c511005640.target)
+	e1:SetOperation(c511005640.operation)
+	c:RegisterEffect(e1)
+end
+
+function c511005640.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetActivityCount(tp,ACTIVITY_ATTACK)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	e:GetHandler():RegisterEffect(e1)
+end
+
+function c511005640.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+
+function c511005640.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e)  and tc:IsRelateToEffect(e) and Duel.Remove(tc,tc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)~=0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+		e1:SetReset(RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN,2)
+		e1:SetCountLimit(1)
+		e1:SetCondition(c511005640.retcon)
+		e1:SetOperation(c511005640.retop)
+		e1:SetLabel(1)
+		e1:SetLabelObject(tc)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function c511005640.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer()
+end
+function c511005640.retop(e,tp,eg,ep,ev,re,r,rp)
+	local t=e:GetLabel()
+	if t==1 then e:SetLabel(0)
+	else Duel.ReturnToField(e:GetLabelObject())	end
+end
