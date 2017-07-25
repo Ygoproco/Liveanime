0acc59536e0ef174d82d238f11aca867877303c3
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-07-25 09:35:57 +0800

    DB Merge for FlameFlash's cards + fixes
    Fixes for:
    * Galaxy Knight Anime
    * Anime Pumpking

--- script/c511013032.lua
+++ script/c511013032.lua
@@ -1,4 +1,5 @@
 --Galaxy Knight
+--fixed by MLD
 function c511013032.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
@@ -11,25 +12,19 @@ function c511013032.initial_effect(c)
 	--cannot attack
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
 	e2:SetCondition(c511013032.atcon)
 	c:RegisterEffect(e2)
 end
-function c511013032.cfilter(c)
-	return c:IsFaceup() and c:IsCode(35950025)
-end
-function c511013032.sfilter(c)
+function c511013032.cfilter(c,code)
 	return c:IsFaceup() and c:IsCode(code)
 end
 function c511013032.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c511013032.cfilter,c:GetControler(),LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c511013032.cfilter,c:GetControler(),LOCATION_MZONE,0,1,nil,35950025)
 end
 function c511013032.atcon(e)
-    if c==nil then return true end
-	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-	    and Duel.IsExistingMatchingCard(c511013032.sfilter,c:GetControler(),LOCATION_MZONE,0,1,c:GetCode())
+	local c=e:GetHandler()
+ 	return Duel.IsExistingMatchingCard(c511013032.cfilter,c:GetControler(),LOCATION_MZONE,0,1,c,c:GetCode())
 end
-
-
