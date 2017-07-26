38e1c07f20af86dad0d57bc940a03d6db46b7a05
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-07-26 18:11:27 +0800

    DB Merge for FlameFlash's cards + fixes (#275)
    * DB Merge for FlameFlash's cards + fixes
    
    Fixes for:
    * Galaxy Knight Anime
    * Anime Pumpking
    
    * Valkyrie Brunhilde fix
    
    * Name Update
    
    Moon Protector => Penumbral Keeper
    Sun Protector => Luminous Keeper
    Protector Adoration => Admiration of the Keepers

--- script/c511001147.lua
+++ script/c511001147.lua
@@ -45,6 +45,7 @@ function c511001147.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(tc:GetAttack()*0.1)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
 		tc:RegisterEffect(e1)
 		tc=g:GetNext()
 	end
