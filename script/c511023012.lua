7b87f97c8899de506a97bb077a2dd62b60c776c1
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-07-28 01:07:15 +0800

    Fixes (#276)
    Umbral Horror Will o' Wisp
    Sage's Stone

--- script/c511023012.lua
+++ script/c511023012.lua
@@ -1,4 +1,5 @@
 --Umbral Horror Will o' the Wisp
+--fixed by MLD
 function c511023012.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(511023012,0))
@@ -14,20 +15,19 @@ function c511023012.initial_effect(c)
 end
 function c511023012.filter(c,clv)
 	local lv=c:GetLevel()
-	return c:IsSetCard and lv~=0 and lv~=clv
-		and (c:IsLocation(LOCATION_MZONE) and c:IsFaceup())
+	return c:IsFaceup() and lv>0 and lv~=clv
 end
 function c511023012.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE+) and c511023012.filter(chkc,e:GetHandler():GetLevel()) end
-	if chk==0 then return Duel.IsExistingTarget(c511023012.filter,tp,LOCATION_MZONE+,0,1,e:GetHandler(),e:GetHandler():GetLevel()) end
+	local c=e:GetHandler()
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and chkc~=c and c511023012.filter(chkc,c:GetLevel()) end
+	if chk==0 then return Duel.IsExistingTarget(c511023012.filter,tp,LOCATION_MZONE,0,1,c,c:GetLevel()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,c511023012.filter,tp,LOCATION_MZONE+,0,1,1,e:GetHandler(),e:GetHandler():GetLevel())
+	Duel.SelectTarget(tp,c511023012.filter,tp,LOCATION_MZONE,0,1,1,c,c:GetLevel())
 end
 function c511023012.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and c:IsFaceup() and tc:IsRelateToEffect(e)
-		and (not tc:IsLocation(LOCATION_MZONE) or tc:IsFaceup()) then
+	if c:IsRelateToEffect(e) and c:IsFaceup() and tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
