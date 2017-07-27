7cca2bf2421015f6291c674523c92aa1d7a49d8b
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-07-27 12:06:10 +0800

    Fixes
    Umbral Horror Will o' Wisp
    Sage's Stone

--- script/c511777009.lua
+++ script/c511777009.lua
@@ -1,57 +1,85 @@
 --アポピスの化神
+--fixed by MLD
 function c511777009.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(c511777009.condition)
+	e1:SetTarget(c511777009.target)
 	e1:SetOperation(c511777009.activate)
 	c:RegisterEffect(e1)
-	--DarkMagician
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetCode(EFFECT_CHANGE_CODE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(46986414)
-	c:RegisterEffect(e2)
-	--Epsendtograve?
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(511777009,0))
-	e3:SetCategory(CATEGORY_TOGRAVE)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetCode(EVENT_PHASE+PHASE_END)
-	e3:SetCondition(c511777009.tgcon)
-	e3:SetTarget(c511777009.tgtg)
-	e3:SetOperation(c511777009.tgop)
-	c:RegisterEffect(e3)
-
 end
 function c511777009.cfilter(c)
 	return c:IsFaceup() and c:IsCode(38033121)
 end
 function c511777009.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c511777009.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
+	return Duel.IsExistingMatchingCard(c511777009.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+end
+function c511777009.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 end
 function c511777009.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
-	Duel.SpecialSummonComplete()
+	if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.MoveToField(c,tp,tp,LOCATION_MZONE,POS_FACEUP,true) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_RACE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(RACE_SPELLCASTER)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e1,true)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e2:SetValue(ATTRIBUTE_DARK)
+		c:RegisterEffect(e2,true)
+		local e3=e1:Clone()
+		e3:SetCode(EFFECT_CHANGE_LEVEL)
+		e3:SetValue(7)
+		c:RegisterEffect(e3,true)
+		local e4=e1:Clone()
+		e4:SetCode(EFFECT_SET_ATTACK)
+		e4:SetValue(2500)
+		c:RegisterEffect(e4,true)
+		local e5=e1:Clone()
+		e5:SetCode(EFFECT_SET_DEFENSE)
+		e5:SetValue(2100)
+		c:RegisterEffect(e5,true)
+		local e6=e1:Clone()
+		e6:SetCode(EFFECT_CHANGE_CODE)
+		e6:SetValue(46986414)
+		c:RegisterEffect(e6,true)
+		local e7=e1:Clone()
+		e7:SetCode(EFFECT_ADD_TYPE)
+		e7:SetValue(TYPE_MONSTER)
+		c:RegisterEffect(e7,true)
+		local fid=c:GetFieldID()
+		c:RegisterFlagEffect(51177709,RESET_EVENT+0x1fe0000,0,1,fid)
+		local e8=Effect.CreateEffect(c)
+		e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e8:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e8:SetCode(EVENT_PHASE+PHASE_END)
+		e8:SetCountLimit(1)
+		e8:SetLabel(fid)
+		e8:SetLabelObject(c)
+		e8:SetCondition(c511777009.tgcon)
+		e8:SetOperation(c511777009.tgop)
+		e8:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e8,tp)
+		c:SetStatus(STATUS_FORM_CHANGED,true)
+	end
 end
 function c511777009.tgcon(e,tp,eg,ep,ev,re,r,rp)
-	return bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL
-end
-function c511777009.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffectLabel(51177709)==e:GetLabel() then
+		return true
+	else
+		e:Reset()
+		return false
+	end
 end
 function c511777009.tgop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		Duel.SendtoGrave(c,REASON_EFFECT)
-	end
+	local tc=e:GetLabelObject()
+	Duel.SendtoGrave(tc,REASON_EFFECT)
 end
