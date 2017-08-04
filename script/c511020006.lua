68bcd8e87a1ae277ac1b03fe65c8a2f1b748142e
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-08-04 18:22:18 +0800

    Fixes (#285)
    Shockwave and Anime Exploder Dragon

--- script/c511020006.lua
+++ script/c511020006.lua
@@ -1,4 +1,6 @@
+--ボマー・ドラゴン
 --Exploder Dragon (Anime)
+--fixed by MLD
 function c511020006.initial_effect(c)
 	--Destroy the Monster
 	local e1=Effect.CreateEffect(c)
@@ -10,38 +12,27 @@ function c511020006.initial_effect(c)
 	e1:SetTarget(c511020006.target)
 	e1:SetOperation(c511020006.operation)
 	c:RegisterEffect(e1)
-	--Damage 0
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_NO_BATTLE_DAMAGE)
-	e2:SetCondition(c511020006.damcon)
-	e2:SetValue(1)
-	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
 	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e3:SetCondition(c511020006.damcon)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
 end
 function c511020006.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
-		and e:GetHandler():GetReasonCard():IsRelateToBattle()
+		and e:GetHandler():GetReasonCard() and e:GetHandler():GetReasonCard():IsRelateToBattle()
 end
 function c511020006.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local rc=e:GetHandler():GetReasonCard()
-	rc:CreateEffectRelation(e)
+	Duel.SetTargetCard(rc)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,rc,1,0,0)
 end
 function c511020006.operation(e,tp,eg,ep,ev,re,r,rp)
 	local rc=e:GetHandler():GetReasonCard()
-	if rc:IsRelateToEffect(e) then
+	if rc and rc:IsRelateToEffect(e) then
 		Duel.Destroy(rc,REASON_EFFECT)
 	end
-end
-function c511020006.damcon(e)
-	local phase=Duel.GetCurrentPhase()
-	return (phase==PHASE_DAMAGE or phase==PHASE_DAMAGE_CAL)
-	and Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler()
 end
