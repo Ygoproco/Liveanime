41773148ea5bfcaac003ca7d98e69d6e77608f6b
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-08-08 22:34:04 +0800

    Name change and hint
    Dreamguy => Dreamer
    Whim Witch Hint for Special Summon

--- script/c511009072.lua
+++ script/c511009072.lua
@@ -6,6 +6,7 @@ function c511009072.initial_effect(c)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetDescription(2)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_PZONE)
