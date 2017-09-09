6d0fd5f110b9ddf0152a4222a9615a8677fae02f
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-09-09 12:15:21 -0300

    Update c511004408.lua

--- script/c511004408.lua
+++ script/c511004408.lua
@@ -14,17 +14,17 @@ function c511004408.tg(e,tp,eg,ev,ep,re,r,rp,chk)
 	local d=Duel.GetAttackTarget()
 	local t1=Duel.CheckEvent(EVENT_ATTACK_ANNOUNCE) and a and d and d:IsAttribute(ATTRIBUTE_DARK) and a:IsControler(1-tp)
 	local t2=not Duel.IsPlayerAffectedByEffect(tp,59822133) and Duel.GetLocationCount(tp,LOCATION_MZONE)>2 
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,123456789,0x10f3,0x4011,0,0,1,RACE_PLANT,ATTRIBUTE_DARK)
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,511004422,0x10f3,0x4011,0,0,1,RACE_PLANT,ATTRIBUTE_DARK)
 	if chk==0 then return t1 or t2 end
 	e:SetCategory(0)
 	e:SetProperty(0)
 	local opt=0
 	if t1 and t2 then
-		opt=Duel.SelectOption(tp,aux.Stringid(123456789,0),aux.Stringid(123456789,1),aux.Stringid(123456789,2))
+		opt=Duel.SelectOption(tp,aux.Stringid(67284107,0),aux.Stringid(33883834,0),aux.Stringid(37390589,2))
 	elseif t1 then
-		opt=Duel.SelectOption(tp,aux.Stringid(123456789,0))
+		opt=Duel.SelectOption(tp,aux.Stringid(67284107,0))
 	else
-		opt=Duel.SelectOption(tp,aux.Stringid(123456789,1))+1
+		opt=Duel.SelectOption(tp,aux.Stringid(33883834,0))+1
 	end
 	if opt==0 or opt==2 then
 		e:SetCategory(e:GetCategory()+CATEGORY_DESTROY)
@@ -61,9 +61,9 @@ function c511004408.op(e,tp,eg,ev,ep,re,r,rp)
 	if opt==1 or opt==2 then
 		if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
 		if Duel.GetLocationCount(tp,LOCATION_MZONE)>2 
-			and Duel.IsPlayerCanSpecialSummonMonster(tp,123456789,0x10f3,0x4011,0,0,1,RACE_PLANT,ATTRIBUTE_DARK) then
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,511004422,0x10f3,0x4011,0,0,1,RACE_PLANT,ATTRIBUTE_DARK) then
 			for i=1,3 do
-				local token=Duel.CreateToken(tp,123456789)
+				local token=Duel.CreateToken(tp,511004422)
 				Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
 			end
 			Duel.SpecialSummonComplete()
