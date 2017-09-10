cd9c445d120546bc806ac378a18b671646030d5a
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-09-10 19:44:30 +0800

    Fixes (#325)
    * Fixes
    
    Phantom Knights' Fog Blade
    Performapal Nightmare Knight
    Z-ONE Boss Duel cards
    D-HERO Dystopia update
    Wonder Balloons Anime
    Blackwing - Sohaya the Early Summer Rain
    
    * Future Battle fix
    
    * Attempt 2

--- script/c511005646.lua
+++ script/c511005646.lua
@@ -1,5 +1,6 @@
 --wonder balloons (Anime)
 --Scripted by GameMaster(GM)
+--fixed by MLD
 function c511005646.initial_effect(c)
 	c:EnableCounterPermit(0x32)
 	--Activate
@@ -9,77 +10,63 @@ function c511005646.initial_effect(c)
 	c:RegisterEffect(e1)
 	--counter
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(511005646,0))
+	e2:SetDescription(aux.Stringid(3070049,0))
 	e2:SetCategory(CATEGORY_COUNTER)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1)
-	e2:SetCost(c511005646.cost)
-	e2:SetTarget(c511005646.target)
-	e2:SetOperation(c511005646.operation)
+	e2:SetCost(c511005646.ctcost)
+	e2:SetTarget(c511005646.cttg)
+	e2:SetOperation(c511005646.ctop)
 	c:RegisterEffect(e2)
 	--send to grave -1000 for each counter
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(511005646,1))
+	e3:SetDescription(aux.Stringid(21521304,0))
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetCost(c511005646.cost3)
-	e3:SetCondition(c511005646.con3)
-	e3:SetCountLimit(1)
-	e3:SetTarget(c511005646.target3)
-	e3:SetOperation(c511005646.operation3)
+	e3:SetCost(c511005646.atkcost)
+	e3:SetTarget(c511005646.atktg)
+	e3:SetOperation(c511005646.atkop)
 	c:RegisterEffect(e3)
 end
-
-function c511005646.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c511005646.ctcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
-	local ct=Duel.DiscardHand(tp,Card.IsAbleToGraveAsCost,1,60,REASON_COST)
-	e:SetLabel(ct)
+	Duel.DiscardHand(tp,Card.IsAbleToGraveAsCost,1,1,REASON_COST)
 end
-
-function c511005646.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c511005646.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,e:GetLabel(),0,0x32)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x32)
 end
-function c511005646.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+function c511005646.ctop(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		c:AddCounter(0x32,e:GetLabel())
+		c:AddCounter(0x32,1)
 	end
 end
-
-function c511005646.con3(e,c)
-local c=e:GetHandler()  
-	if c:GetCounter(0x32)>0 then 
-return true
-else return false end
-end
-
-function c511005646.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	e:SetLabel(e:GetHandler():GetCounter(0x32))
+function c511005646.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:GetCounter(0x32)>0 and c:IsAbleToGraveAsCost() end
+	e:SetLabel(c:GetCounter(0x32))
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
-
-function c511005646.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+function c511005646.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	local ct=e:GetLabel()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	e:SetLabel(0)
+	Duel.SetTargetParam(ct)
 end
-
-function c511005646.operation3(e,tp,eg,ep,ev,re,r,rp,chk)
+function c511005646.atkop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		e1:SetValue(e:GetLabel()*-1000)
+		e1:SetValue(ct*-1000)
 		tc:RegisterEffect(e1)
 	end
 end
-
-
