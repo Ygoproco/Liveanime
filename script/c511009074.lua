87c91e39b8ee4c1f488cbd4b72d43b463b75d127
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-09-10 08:44:19 -0300

    small fix (#323)
    typo

--- script/c511009074.lua
+++ script/c511009074.lua
@@ -88,7 +88,7 @@ function c511009074.disop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c511009074.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	if g:GetCount()>0 and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+	if g:GetCount()>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
 		Duel.NegateEffect(ev)
 	end
 end
