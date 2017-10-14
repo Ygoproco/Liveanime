772a251da584cac4571562e7cb193473b44a722e
ahtelel <32773013+ahtelel@users.noreply.github.com>
2017-10-14 18:26:59 +0700

    Update c511247006.lua

--- script/c511247006.lua
+++ script/c511247006.lua
@@ -1,6 +1,6 @@
 --Dark Cure (Anime)
 function c511247006.initial_effect(c)
-		local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
