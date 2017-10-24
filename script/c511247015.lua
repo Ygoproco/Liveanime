2b4efcbd71434a73aee5e48e2fbf66825cf86d98
ahtelel <32773013+ahtelel@users.noreply.github.com>
2017-10-24 15:00:24 +0700

    Update c511247015.lua

--- script/c511247015.lua
+++ script/c511247015.lua
@@ -11,7 +11,7 @@ function c511247015.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c511247015.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp and Duel.GetAttackTarget()==nil
+	return Duel.GetTurnPlayer()~=tp and Duel.GetAttackTarget()==nil and Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0,nil)==0
 end
 function c511247015.filter(c)
 	return c:IsSetCard(0x21) and c:IsAbleToHand() and not c:IsCode(64187086)
