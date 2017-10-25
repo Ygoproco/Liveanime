be662daa4e886f72739be1c1aacebb36533dc1ef
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-10-26 07:59:12 +0800

    recheck

--- script/c511009106.lua
+++ script/c511009106.lua
@@ -40,7 +40,7 @@ function c511009106.operation(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetValue(1)
 			e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
 			c:RegisterEffect(e1)
-      local e2=Effect.CreateEffect(c)
+			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 			e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
 			e2:SetOperation(c511009106.damop)
@@ -58,7 +58,7 @@ function c511009106.filter2(c,e,tp,m,f,gc)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,gc)
 end
 function c511009106.damop(e,tp,eg,ep,ev,re,r,rp)
-  Duel.ChangeBattleDamage(tp,0)
+	Duel.ChangeBattleDamage(tp,0)
 end
 function c511009106.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -79,36 +79,36 @@ function c511009106.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c511009106.spop(e,tp,eg,ep,ev,re,r,rp)
-  local c=e:GetHandler()
-  if c:IsFacedown() or not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) then return end
-  local mg1=Duel.GetFusionMaterial(tp):Filter(c511009106.filter1,nil,e)
-  local sg1=Duel.GetMatchingGroup(c511009106.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,c)
-    local mg2=nil
-    local sg2=nil
-    local ce=Duel.GetChainMaterial(tp)
-    if ce~=nil then
-        local fgroup=ce:GetTarget()
-        mg2=fgroup(ce,e,tp)
-        local mf=ce:GetValue()
-        sg2=Duel.GetMatchingGroup(c511009106.spfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,c)
-    end
-    if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-        local sg=sg1:Clone()
-        if sg2 then sg:Merge(sg2) end
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local tg=sg:Select(tp,1,1,nil)
-        local tc=tg:GetFirst()
-        if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-            local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,c)
-            tc:SetMaterial(mat1)
-            Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-            Duel.BreakEffect()
-            Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-        else
-            local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,c)
-            local fop=ce:GetOperation()
-            fop(ce,e,tp,tc,mat2)
-        end
-        tc:CompleteProcedure()
-    end
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) then return end
+	local mg1=Duel.GetFusionMaterial(tp):Filter(c511009106.filter1,nil,e)
+	local sg1=Duel.GetMatchingGroup(c511009106.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,c)
+	local mg2=nil
+	local sg2=nil
+	local ce=Duel.GetChainMaterial(tp)
+	if ce~=nil then
+		local fgroup=ce:GetTarget()
+		mg2=fgroup(ce,e,tp)
+		local mf=ce:GetValue()
+		sg2=Duel.GetMatchingGroup(c511009106.spfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,c)
+	end
+	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
+		local sg=sg1:Clone()
+		if sg2 then sg:Merge(sg2) end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local tg=sg:Select(tp,1,1,nil)
+		local tc=tg:GetFirst()
+		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
+			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,c)
+			tc:SetMaterial(mat1)
+			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+			Duel.BreakEffect()
+			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+		else
+			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,c)
+			local fop=ce:GetOperation()
+			fop(ce,e,tp,tc,mat2)
+		end
+		tc:CompleteProcedure()
+	end
 end
