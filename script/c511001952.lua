96a7e2f8758f6e5cd07fe04fecceebea80019470
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-11-01 18:53:40 +0800

    Ultimitl fix

--- script/c511001952.lua
+++ script/c511001952.lua
@@ -113,61 +113,65 @@ function c511001952.val(e,c)
 	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,LOCATION_MZONE)*1000
 end
 function c511001952.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,90884404,0,0x4011,100,0,1,RACE_FIEND,ATTRIBUTE_DARK)) 
-		or (Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 
-		and Duel.IsPlayerCanSpecialSummonMonster(1-tp,90884404,0,0x4011,100,0,1,RACE_FIEND,ATTRIBUTE_DARK)) end
-	local ft=(Duel.GetLocationCount(tp,LOCATION_MZONE)+Duel.GetLocationCount(1-tp,LOCATION_MZONE))
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)+Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,90884404,0,0x4011,100,0,1,RACE_FIEND,ATTRIBUTE_DARK) end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)+Duel.GetLocationCount(1-tp,LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,ft,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ft,0,0)
 end
 function c511001952.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerCanSpecialSummonMonster(tp,90884404,0,0x4011,100,0,1,RACE_FIEND,ATTRIBUTE_DARK) then return end
+	local ct=0
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
-	if ft<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,90884404,0,0x4011,100,0,1,RACE_FIEND,ATTRIBUTE_DARK) then return end
-	for i=1,ft do
-		local token=Duel.CreateToken(tp,90884404)
-		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SET_BASE_ATTACK)
-		e1:SetValue(100)
-		e1:SetReset(RESET_EVENT+0xfe0000)
-		token:RegisterEffect(e1)
+	if ft>0 then
+		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+		for i=1,ft do
+			local token=Duel.CreateToken(tp,90884404)
+			if Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then ct=1 end
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_BASE_ATTACK)
+			e1:SetValue(100)
+			e1:SetReset(RESET_EVENT+0xfe0000)
+			token:RegisterEffect(e1)
+		end
 	end
 	local ft2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)
-	if ft2>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then ft2=1 end
-	if ft2<=0 or not Duel.IsPlayerCanSpecialSummonMonster(1-tp,90884404,0,0x4011,0,0,1,RACE_FIEND,ATTRIBUTE_DARK) then return end
-	for i=1,ft2 do
-		local token=Duel.CreateToken(tp,90884404)
-		Duel.SpecialSummonStep(token,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SET_BASE_ATTACK)
-		e1:SetValue(100)
-		e1:SetReset(RESET_EVENT+0xfe0000)
-		token:RegisterEffect(e1)
+	if ft2>0 then
+		if Duel.IsPlayerAffectedByEffect(tp,59822133) then
+			if ct>0 then Duel.SpecialSummonComplete() return end
+			ft2=1
+		end
+		for i=1,ft2 do
+			local token=Duel.CreateToken(tp,90884404)
+			Duel.SpecialSummonStep(token,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_BASE_ATTACK)
+			e1:SetValue(100)
+			e1:SetReset(RESET_EVENT+0xfe0000)
+			token:RegisterEffect(e1)
+		end
 	end
 	Duel.SpecialSummonComplete()
 end
 function c511001952.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsOnField() and c:IsFaceup()
-		and Duel.IsExistingMatchingCard(Card.IsDestructable,tp,0,LOCATION_MZONE,1,nil) end
-	if Duel.SelectYesNo(tp,aux.Stringid(511001952,0)) then return true
-	else return false end
+		and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+	return Duel.SelectYesNo(tp,aux.Stringid(511001952,0))
 end
 function c511001952.desrepop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RaiseSingleEvent(e:GetHandler(),511001952,re,r,rp,0,0)
 end
 function c511001952.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetCount()*200)
 end
 function c511001952.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
 	local ct=Duel.Destroy(g,REASON_EFFECT)
 	if ct>0 then
 		Duel.Damage(1-tp,ct*200,REASON_EFFECT)
